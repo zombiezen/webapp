7a705ea8bb6233648667f2dac3e14e85635df5b8
Ross Light <ross@zombiezen.com>
2012-12-01 11:45:07 -0800

    Add MultiError type

--- data.go
+++ data.go
@@ -2,6 +2,7 @@ package webapp
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -15,4 +16,29 @@ const (
 func JSONResponse(w http.ResponseWriter, v interface{}) error {
 	w.Header().Set("Content-Type", JSONType)
 	return json.NewEncoder(w).Encode(v)
+}
+
+// A MultiError is returned by operations that have errors on particular elements.
+// This is functionally identical to appengine.MultiError.
+type MultiError []error
+
+func (e MultiError) Error() string {
+	msg, n := "", 0
+	for _, err := range e {
+		if err != nil {
+			if n == 0 {
+				msg = err.Error()
+			}
+			n++
+		}
+	}
+	switch n {
+	case 0:
+		return "0 errors"
+	case 1:
+		return msg
+	case 2:
+		return msg + " (and 1 other error)"
+	}
+	return fmt.Sprintf("%s (and %d other errors)", msg, n-1)
 }
