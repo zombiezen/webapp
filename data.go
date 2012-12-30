b78262c0bd907113f6f7c5d3024c3daae82d13e6
Ross Light <ross@zombiezen.com>
2012-12-30 10:59:57 -0800

    Add common header constants and response functions

--- data.go
+++ data.go
@@ -4,6 +4,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+)
+
+// Common HTTP headers
+const (
+	HeaderAllow              = "Allow"
+	HeaderContentDisposition = "Content-Disposition"
+	HeaderContentEncoding    = "Content-Encoding"
+	HeaderContentLength      = "Content-Length"
+	HeaderContentType        = "Content-Type"
 )
 
 // Content types
@@ -11,6 +22,22 @@ const (
 	HTMLType = "text/html; charset=utf-8"
 	JSONType = "application/json; charset=utf-8"
 )
+
+// MethodNotAllowed replies to a request with an HTTP 405 method not allowed error.
+func MethodNotAllowed(w http.ResponseWriter, methods ...string) {
+	w.Header().Set(HeaderAllow, strings.Join(methods, ", "))
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
+// ContentLength sets the Content-Length header to size.
+func ContentLength(h http.Header, size int64) {
+	h.Set(HeaderContentLength, strconv.FormatInt(size, 10))
+}
+
+// Attachment sets the Content-Disposition header to an attachment with the given file name.
+func Attachment(h http.Header, base, ext string) {
+	h.Set(HeaderContentDisposition, "attachment; filename="+base+"."+ext)
+}
 
 // JSONResponse writes a JSON value to w, setting the Content-Type.
 func JSONResponse(w http.ResponseWriter, v interface{}) error {
