c220928197b7b92e732f0a383493fc9bbc5d5b9b
Ross Light <ross@zombiezen.com>
2013-02-02 13:38:25 -0800

    Add Cycle function

--- template.go
+++ template.go
@@ -9,9 +9,18 @@ import (
 // AddFuncs adds the package's template functions.
 func AddFuncs(t *template.Template, router *mux.Router) {
 	t.Funcs(template.FuncMap{
-		"path": RoutePath(router),
-		"url":  RouteURL(router),
+		"path":  RoutePath(router),
+		"url":   RouteURL(router),
+		"cycle": Cycle,
 	})
+}
+
+// Cycle cycles among the given strings given an index.
+func Cycle(i int, arg0 string, args ...string) string {
+	if j := i % (len(args) + 1); j != 0 {
+		return args[j-1]
+	}
+	return arg0
 }
 
 // RoutePath returns a function suitable for a template that returns the path
